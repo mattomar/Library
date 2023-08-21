cb32a87430ef0375a754d937c16cd17531d7d3db
mattomar <moddathero@gmail.com>
2023-08-21 16:55:54 +0700

    added the delete and read button to the card

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+
+
 function book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -10,97 +12,155 @@ function book(title, author, pages, read) {
     };
 }
 
+
+
+
 function toggleForm() {
     const form = document.getElementById("bookForm");
 
     if (form.style.display === "none" || form.style.display === "") {
-        // Show the form
         form.style.display = "block";
-        form.style.maxWidth = "400px"; // Set a maximum width for the form
-        form.style.backgroundColor = "#b9b3b3"; // Set background color
-        form.style.padding = "20px"; // Set padding
-        form.style.textAlign = "center"; // Center-align text
+        form.style.opacity = "0";
+        form.style.transition = "opacity 0.3s ease-in-out";
+        form.style.width = "30%";
+        form.style.backgroundColor = "rgba(185, 179, 179, 0.8)";
+        form.style.padding = "20px";
+        form.style.textAlign = "center";
         form.style.height = "400px";
         form.style.position = "absolute";
-        form.style.top = "50%"; // Center vertically
-        form.style.left = "50%"; // Center horizontally
-        form.style.transform = "translate(-50%, -50%)"; // Center the form
+        form.style.top = "50%";
+        form.style.left = "50%";
+        form.style.transform = "translate(-50%, -50%)";
 
-
-        // Style input fields
-        const inputs = form.querySelectorAll("input[type='text'], input[type='number']");
+        const inputs = form.querySelectorAll("input[type='text'], input[type='number'], select");
         inputs.forEach(input => {
-            input.style.width = "90%"; // Reduce input field width to prevent overflow
-            input.style.padding = "10px"; // Input field padding
-            input.style.marginBottom = "10px"; // Spacing between input fields
-            input.style.border = "1px solid #ccc"; // Add a border to input fields
-            input.style.borderRadius = "5px"; // Add some border radius for rounded corners
-            input.style.fontSize = "14px"; // Set the font size for input fields
+            input.style.width = "90%";
+            input.style.padding = "10px";
+            input.style.marginBottom = "10px";
+            input.style.border = "1px solid #ccc";
+            input.style.borderRadius = "5px";
+            input.style.fontSize = "14px";
         });
 
-        // Style labels
         const labels = form.querySelectorAll("label");
         labels.forEach(label => {
-            label.style.display = "block"; // Make labels display as blocks (one below the other)
-            label.style.fontWeight = "bold"; // Make labels bold for emphasis
+            label.style.display = "block";
+            label.style.fontWeight = "bold";
         });
 
-        // Style the submit button
         const submitButton = form.querySelector("button[type='submit']");
-        submitButton.style.backgroundColor = "#3498db"; // Button background color
-        submitButton.style.color = "white"; // Button text color
-        submitButton.style.padding = "8px 16px"; // Button padding
-        submitButton.style.border = "none"; // Remove button border
-        submitButton.style.borderRadius = "5px"; // Add some border radius for rounded corners
-        submitButton.style.fontSize = "16px"; // Set the font size for the button
+        submitButton.style.backgroundColor = "#3498db";
+        submitButton.style.color = "white";
+        submitButton.style.padding = "8px 16px";
+        submitButton.style.border = "none";
+        submitButton.style.borderRadius = "5px";
+        submitButton.style.fontSize = "16px";
+        submitButton.style.cursor = "pointer";
+        submitButton.style.transition = "background-color 0.3s ease";
+        submitButton.addEventListener("mouseenter", () => {
+            submitButton.style.backgroundColor = "#2980b9";
+        });
+        submitButton.addEventListener("mouseleave", () => {
+            submitButton.style.backgroundColor = "#3498db";
+        });
+
+        setTimeout(() => {
+            form.style.opacity = "1";
+        }, 10);
     } else {
-        // Hide the form
-        form.style.display = "none";
+        form.style.opacity = "0";
+        setTimeout(() => {
+            form.style.display = "none";
+        }, 300);
     }
 }
+
+
 document.getElementById("showFormButton").addEventListener("click", toggleForm);
 
-document.getElementById("bookForm").addEventListener("submit", function(event) {
+document.getElementById("bookForm").addEventListener("submit", function (event) {
     event.preventDefault();
     addBookToLibrary();
     toggleForm();
 });
 
-function createBookCard(book) {
+
+
+
+function createBookCard(book, index) {
     const card = document.createElement("div");
     card.classList.add("book");
     card.innerHTML = `
         <h2>${book.title}</h2>
         <p>Author: ${book.author}</p>
         <p>Pages: ${book.pages}</p>
-        <p>Read: ${book.read}</p>
+        <p>Read: <span id="read-status-${index}">${book.read}</span></p>
+        <button class="toggle-read-button" data-index="${index}">Toggle Read</button>
+        <button class="remove-button" data-index="${index}">Remove</button>
     `;
+
+    const toggleReadButton = card.querySelector(".toggle-read-button");
+    toggleReadButton.addEventListener("click", function () {
+        toggleReadStatus(index);
+    });
+
+    card.style.margin = "20px";
+
+    const removeButton = card.querySelector(".remove-button");
+    removeButton.addEventListener("click", function () {
+        removeBook(index);
+    });
+
     return card;
 }
+
+
+
+
+
+function toggleReadStatus(index) {
+    const readStatusElement = document.getElementById(`read-status-${index}`);
+    const currentStatus = readStatusElement.innerText;
+
+    const newStatus = currentStatus === "Yes" ? "No" : "Yes";
+    readStatusElement.innerText = newStatus;
+
+    myLibrary[index].read = newStatus;
+}
+
+
+
+
 function addBookToLibrary() {
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const pages = document.getElementById("pages").value;
     const read = document.getElementById("read").value;
 
-    // Create a new book first
     const newBook = new book(title, author, pages, read);
 
-    // Create a card for the new book
-    const card = createBookCard(newBook);
+    const card = createBookCard(newBook, myLibrary.length);
 
-    // Append the card to the library container
     const libraryContainer = document.querySelector(".library");
     libraryContainer.appendChild(card);
 
-    // Push the new book to the library array
     myLibrary.push(newBook);
 
-    // Clear the form input fields
     document.getElementById("title").value = "";
     document.getElementById("author").value = "";
     document.getElementById("pages").value = "";
     document.getElementById("read").value = "";
+}
 
-    console.log(myLibrary);
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+
+    const libraryContainer = document.querySelector(".library");
+    libraryContainer.removeChild(libraryContainer.children[index]);
+
+    const removeButtons = document.querySelectorAll(".remove-button");
+    removeButtons.forEach((button, i) => {
+        button.dataset.index = i;
+    });
 }
